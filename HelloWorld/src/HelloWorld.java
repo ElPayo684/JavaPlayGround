bcd5bb6f794490b044c0f39161c697a108c233bd
Eduardo <eduardo_lg98@hotmail.com>
2024-03-19 13:54:25 -0600

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    // Todo en Java es una clase
+    // Para cualquier aplicación en Java, se aplica una clase publica:
+    public static void main(String[] args){ // psvm + tab
+    // Void es un retorno vacio, no devuelve ningun valor, solo ejecuta un proceso 
+        System.out.println("Hello world! I'm in Java");  // clase System de java | atributo out.println(para imprimir un texto)
+    }
+}
---
+++ HelloWorld/src/MyName.java
@@ -0,0 +1,6 @@
+public class MyName {
+    public static void main(String[] args) {
+        String name = "Eddard";
+        System.out.println("Hello, my name is " + name);
+    }    
+}
